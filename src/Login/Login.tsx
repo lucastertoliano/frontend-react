7b407a1c1674164903ab110529f31ad80bbb7a49
lucastertoliano <158801827+lucastertoliano@users.noreply.github.com>
2025-11-01 20:54:54 -0300

    login part 2

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -3,16 +3,17 @@ const Login = () => {
   return (
     <div id="login-container">
         <div id="left-container">
+            <h2 className='title'>Sign In to Developer</h2>
             <form id="login-form" onSubmit={() => {}}>
                 <input type="text" id="username" placeholder="Usuário" />
                 <input type="password" id="password" placeholder="Senha" />
                 
-                <button type="submit">Entrar</button>
+                <button type="submit">Login</button>
             </form>
         </div>
         <div id="right-container">
-            LOREM IPSUM
-            <button type="submit">Entrar</button>
+            <h2 className="welcome-title">Seja bem vindo!</h2>
+            <button type="submit">Login</button>
         </div>
     </div>
   )
