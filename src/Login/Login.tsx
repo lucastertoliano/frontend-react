51b06ff7e587006060f9a40a8e211e0c0a512218
unknown <pedrinho.jpln@gmail.com>
2025-11-03 13:42:29 -0300

    fechei a aula do dia 21 parte 2 e a aulda do dia 23

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,22 +1,48 @@
+import { useState } from 'react';
 import './Login.css'
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const sendRequest = (e: any) => {
+    e.preventDefault();
+    if(email && password){
+      alert(`Login realizado com sucesso!`);
+    }else{
+      alert('Por favor, preencha todos os campos')
+    }
+  }
   return (
     <div id="login-container">
         <div id="left-container">
             <h2 className='title'>Sign In to Developer</h2>
-            <form id="login-form" onSubmit={() => {}}>
-                <input type="text" id="username" placeholder="Usuário" />
-                <input type="password" id="password" placeholder="Senha" />
+        <div className='icon-container'>
+            <button>
+              <i className="fab fa-facebook-f"></i>
+            </button>
+            <button>
+              <i className="fab fa-instagram"></i>
+            </button>
+            <button>
+              <i className="fab fa-linkedin-in"></i>
+            </button>
+        </div>
+            <form id="login-form" onSubmit={(e) => {sendRequest(e)}}>
+                <input type="email" id="username" placeholder="Usuário" onChange ={(e) => {setEmail(e.target.value)}}/> 
+                <input type="password" id="password" placeholder="Senha" onChange ={(e) => {setPassword(e.target.value)}}/>
+            <div className='forgot-password-container'>
+              <a href="#">Esqueceu a senha?</a>
+              <button type="submit">Login</button>
+            </div>
                 
-                <button type="submit">Login</button>
             </form>
         </div>
         <div id="right-container">
             <h2 className="welcome-title">Seja bem vindo!</h2>
-            <button type="submit">Login</button>
+            <p className='welcome-message'>Para continuar, faça seu login</p>
+            <button type="submit">Sign up</button>
         </div>
     </div>
   )
 }
-
 export default Login
