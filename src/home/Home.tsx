d8e96c7bea1c2533752d5a0243aeea9b8972d550
lucastertoliano <158801827+lucastertoliano@users.noreply.github.com>
2025-11-04 13:25:12 -0300

    navbar, avatar, aulas finalizadas

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -3,14 +3,17 @@ import './Home.css';
 const Home = () => {
   return (
     <nav id="home-bar">
-        <div className='avatar'>
-            <img src="src/assets/avatar_icon.png" alt="User Avatar" />
-        </div>
         <div className='links'>
             <a href="/home">Home</a>
             <a href="/about">About</a>
             <a href="/sign-up">Sign Up</a>
-            <a href="/login">Login</a>
+            <a href="/login">Logout</a>
+        </div>
+        <div className='user-info'>
+          <div className='avatar'>
+            <img src="src/assets/avatar_icon.png" alt="User Avatar" />
+          </div>
+          <label>User Name</label>
         </div>
     </nav>
   )
