ce38e5494dfbb255a976e129c3a5f4a70157bdb9
unknown <pedrinho.jpln@gmail.com>
2025-11-07 16:07:52 -0300

    reverti o ultimo commit

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,20 +1,24 @@
 import './Home.css';
-import NavBar from '../navBar/nav.tsx';
 
 const Home = () => {
   return (
-    <div>
-      <NavBar />
-      
-      <div id='procurar-container'>
+    <nav id="home-bar">
+        <div className='links'>
+            <a href="/home">Home</a>
+            <a href="/gerenciamentoProduto">Produtos</a>
+            <a href="/promocao">Promoção</a>
+            <a href="/gerenciamentoUsuario">Usuários</a>
+            <a href="/detalhesUsuario">Detalhes dos Usuários</a>
+            <a href="/login">Sair</a>
+        </div>
+        <div className='user-info'>
+          <div className='avatar'>
+            <img src="src/assets/avatar_icon.png" alt="User Avatar" />
+          </div>
+          <label>User Name</label>
+        </div>
+    </nav>
+  )
+}
 
-      </div>
-      <div>
-
-      </div>
-
-    </div>
-  );
-};
-
-export default Home;
+export default Home
